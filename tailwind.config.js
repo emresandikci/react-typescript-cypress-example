de9a0f7ba919cb3113ea8aefe228335b959c0c7d
mr.san <me@emresandikci.com>
2022-11-20 07:35:46 +0300

    chore: add reselect, classnames,framer-motion,react-icons,react-select

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,43 @@
-/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: '300px',
+        tablet: '640px',
+        laptop: '1024px',
+        desktop: '1280px',
+        desktopL: '1440px',
+        desktopXL: '1980px',
+        '4k': '2560px',
+      },
+      zIndex: {
+        scroller: '100',
+        dropdown: '200',
+        modal: '1000',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.shadow-bb': {
+          'box-shadow': '0px 4px 12px rgba(29, 29, 32, 0.08)',
+        },
+        '.shadow-bb-l': {
+          'box-shadow': '-4px 0px 5px 0px rgba(29, 29, 32, 0.08)',
+        },
+        '.shadow-bb-r': {
+          'box-shadow': '4px 0px 5px 0px rgba(29, 29, 32, 0.08)',
+        },
+        '.shadow-bb-t': {
+          'box-shadow': '1px -2px 4px 0px rgba(29, 29, 32, 0.08)',
+        },
+        '.shadow-bb-b': {
+          'box-shadow': '2px 4px 5px 0px  rgba(29, 29, 32, 0.08)',
+        },
+      });
+    }),
+  ],
 };
