df848c38359cd0eebd73cd449e45c99c8d7cbdcc
mr.san <me@emresandikci.com>
2022-11-21 03:48:17 +0300

    feat: add new context states

--- src/containers/commentsContainer/index.tsx
+++ src/containers/commentsContainer/index.tsx
@@ -1,17 +1,26 @@
-import { CreateCommentModal } from 'components';
+import { useContext, createContext, useState } from 'react';
+import { AddTagPopup, CreateCommentModal } from 'components';
 import { useModal } from 'hooks';
-import { useContext, createContext } from 'react';
 import { BaseComponentProps } from 'utils/types';
+import { IComment } from 'utils/types/comment';
 import './index.css';
 
 type CommentContextState = {
   toggleCreateCommentModal: () => void;
   isCreateCommentModalActive: boolean;
+  toggleAddTagPopup: () => void;
+  isAddTagPopupActive: boolean;
+  selectedComment: IComment | null;
+  setSelectedComment: (comment: IComment | null) => void;
 };
 
 const initialContextState: CommentContextState = {
   toggleCreateCommentModal: () => {},
   isCreateCommentModalActive: false,
+  toggleAddTagPopup: () => {},
+  isAddTagPopupActive: false,
+  selectedComment: null,
+  setSelectedComment: () => {},
 };
 
 export const CommentContext = createContext<CommentContextState>(initialContextState);
@@ -22,18 +31,27 @@ interface ICommentsContainer extends BaseComponentProps {
 
 export default function CommentsContainer({ children, innerRef, ...props }: ICommentsContainer) {
   const [isCreateCommentModalActive, toggleCreateCommentModal] = useModal({});
+  const [isAddTagPopupActive, toggleAddTagPopup] = useModal({});
+  const [selectedComment, setSelectedComment] = useState<IComment | null>(null);
 
   return (
     <CommentContext.Provider
       value={{
         toggleCreateCommentModal,
         isCreateCommentModalActive,
+        toggleAddTagPopup,
+        isAddTagPopupActive,
+        selectedComment,
+        setSelectedComment,
       }}
     >
       <div ref={innerRef} className="comments-container" {...props}>
         {children}
       </div>
       {isCreateCommentModalActive && <CreateCommentModal onClose={toggleCreateCommentModal} />}
+      {isAddTagPopupActive && (
+        <AddTagPopup isOpen={isAddTagPopupActive} onClose={toggleAddTagPopup} />
+      )}
     </CommentContext.Provider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import { RouteContainer } from 'containers';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from 'store';
-// import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 function App() {
